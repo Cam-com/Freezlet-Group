28ad4dabcdac266925c5dfc9bebcb827d44a196d
Cam-com <camille@plushg.fr>
2024-06-13 20:49:27 +0200

    rename of files

--- src/routes/CV1/+page.svelte
+++ src/routes/CV1/+page.svelte
@@ -7,10 +7,10 @@
 <script>
 	import Camille_Roux from '$lib/images/Camille_Roux.jpeg';
   import LIL_Logo from '$lib/images/LIL-Logo.jpg';
-  import LIL_Campus from '$lib/images/LiL - Campus.jpg';
+  import LIL_Campus from '$lib/images/LiL-Campus.jpg';
   import LIL_Uniform from '$lib/images/LiL-Uniform.jpeg';
-  import St_Jean_Campus from '$lib/images/St Jean Campus.jpg';
-  import Prepa_Study_Cat from '$lib/images/Prepa Study Cat.jpeg';
+  import St_Jean_Campus from '$lib/images/St-Jean-Campus.jpg';
+  import Prepa_Study_Cat from '$lib/images/Prepa_Study_Cat.jpeg';
   import St_Jean_Logo_Prepa from '$lib/images/St-Jean Logo Prepa.webp';
   import ESSEC_Logo from '$lib/images/ESSEC Logo.jpg';
   import ESSEC_Gala from '$lib/images/ESSEC Gala.jpeg';
