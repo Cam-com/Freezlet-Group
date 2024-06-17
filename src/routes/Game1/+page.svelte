985f4a35ee1f293a600046d35dd8d11989136b02
Cam-com <camille@plushg.fr>
2024-06-17 17:56:54 +0200

    Progress

--- src/routes/Game1/+page.svelte
+++ src/routes/Game1/+page.svelte
@@ -1,4 +1,27 @@
+<script>
+    import { goto } from "$app/navigation";
+</script>
+
+
 <h1>Game 1</h1>
+
+<!-- Navbar -->
+<div class="bg-base-100 justify-between fixed top-20 left-0 right-0">
+    <div class="navbar max-w-3xl mx-auto justify-between">
+        <div>
+            <a href="/" class="btn btn-ghost text-xl">Pokepage</a>
+            <a href="/user" class="btn btn-ghost">My Page</a>
+        </div>
+
+        <div>
+            <button on:click={() => goto("/login")}>Login</button>
+            <span class="text-lg ml-2">cooper@gmail.com</span>
+            <button on:click={() => console.log("logging out")}>Logout</button>
+        </div>
+
+    </div>
+
+</div>
 
 <!--The Flash Card-->
 <div>
