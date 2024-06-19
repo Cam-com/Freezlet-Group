9d5a8461da121c8c97b4a0cf3c54835049ba8e57
Cam-com <camille@plushg.fr>
2024-06-19 17:03:45 +0200

    Second Supabase attempt

--- src/routes/Game1/+page.svelte
+++ src/routes/Game1/+page.svelte
@@ -1,4 +1,20 @@
 <!-- localhost:5137/coopercodes3@gmail.com -->
+<script>
+    import supabase from '$lib/db'
+/*
+    async function getData() {
+        const { data, error } = await supabase
+        .from('games')
+        .select()
+        if (error) throw new Error(error.message)
+  
+        return data
+    }
+*/
+    var Question = "Question 1"
+
+
+</script>
 
 <h1 class="font-extrabold text-3xl">Flash cards</h1>
 
@@ -6,7 +22,7 @@
 <div class="flex items-center justify-center min-h-screen">
     <div class="flex flex-col items-center bg-gray-100 rounded-lg shadow-md">
         <div class="w-72 h-32 p-4 border border-gray-300 bg-white m-5 mb-0">
-            <p class="w-full h-full p-2 text-lg font-bold text-center content-center border border-gray-300 resize-none">Question</p>
+            <p class="w-full h-full p-2 text-lg font-bold text-center content-center border border-gray-300 resize-none">Question { Question }</p>
         </div>
         <div class="w-72 h-20 p-4 border border-gray-300 bg-white m-5 mb-0">
             <textarea class="w-full h-full p-2 border border-gray-300 resize-none" placeholder="Enter traduction here..."></textarea>
