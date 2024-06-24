1c7f47c3de25444e741015112519a2373df26594
fannymf9 <fanny.ferreira@essec.edu>
2024-06-24 18:55:14 +0200

    jeu3modif2

--- src/routes/Game3/+page.svelte
+++ src/routes/Game3/+page.svelte
@@ -26,6 +26,11 @@
     let zoomed3 = false;
     let zoomed4 = false;
     let zoomed5 = false;
+    let zoomed6 = false;
+    let zoomed7 = false;
+    let zoomed8 = false;
+    let zoomed9 = false;
+    let zoomed10 = false;
     var selected = ['nothing','nothing'];
     var position = [0,0];
 
@@ -59,7 +64,7 @@
         zoomed5 = false;
     }
 
-    function toogleZoom4_First() {
+    function toggleZoom4_First() {
         selected[0] = data.words[3][0]
         position[0]= 4
         zoomed4 = !zoomed4;
@@ -69,7 +74,7 @@
         zoomed5 = false;
     }
 
-    function toogleZoom5_First() {
+    function toggleZoom5_First() {
         selected[0] = data.words[4][0]
         position[0]= 5
         zoomed5 = !zoomed5;
@@ -79,6 +84,56 @@
         zoomed4 = false;  
     }  
     
+    function toggleZoom6_First(){
+        selected[1] = Mixwords[0]
+        position[1]=1
+        zoomed6 = !zoomed6;
+        zoomed7 = false
+        zoomed8 = false
+        zoomed9 = false
+        zoomed10 = false
+
+    }    
+
+    function toggleZoom7_First(){
+        selected[1] = Mixwords[1]
+        position[1]=2
+        zoomed7 = !zoomed7;
+        zoomed6 = false
+        zoomed8 = false
+        zoomed9 = false
+        zoomed10 = false
+    }
+
+    function toggleZoom8_First(){
+        selected[1] = Mixwords[2]
+        position[1]=3
+        zoomed8 = !zoomed8;
+        zoomed6 = false
+        zoomed7 = false
+        zoomed9 = false
+        zoomed10 = false
+    }
+
+    function toggleZoom9_First(){
+        selected[1] = Mixwords[3]
+        position[1]=4
+        zoomed9 = !zoomed9;
+        zoomed6 = false
+        zoomed7 = false
+        zoomed8 = false
+        zoomed10 = false
+    }
+
+    function toggleZoom10_First(){
+        selected[1] = Mixwords[4]
+        position[1]=5
+        zoomed10 = !zoomed10;
+        zoomed6 = false
+        zoomed7 = false
+        zoomed8 = false
+        zoomed9 = false
+    }
 
 
 
@@ -95,7 +150,7 @@
         transition: transform 0.3s ease;
     }
 
-    .zoomed1, .zoomed2, .zoomed3, .zoomed4, .zoomed5 {
+    .zoomed1, .zoomed2, .zoomed3, .zoomed4, .zoomed5, .zoomed6, .zoomed7, .zoomed8, .zoomed9, .zoomed10 {
         transform: scale(1.1); /* Adjust the scale value as needed */
         background-color: rgb(238, 244, 49)
     }
@@ -146,19 +201,34 @@
             </div>
 
             <div class="flex flex-col">
-                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0">
+                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0"
+                class:zoomed6
+                on:click={toggleZoom6_First}>
+
                     <p class="w-full h-full p-2 text-center border border-gray-300 resize-none">{ Mixwords[0] }</p>
                 </button>
-                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0">
+                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0"
+                class:zoomed7
+                on:click={toggleZoom7_First}>
                     <p class="w-full h-full p-2 text-center border border-gray-300 resize-none">{ Mixwords[1] }</p>
                 </button>
-                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0">
+
+                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0"
+                class:zoomed8
+                on:click={toggleZoom8_First}>
+
                     <p class="w-full h-full p-2 text-center border border-gray-300 resize-none">{ Mixwords[2] }</p>
                 </button>
-                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0">
+
+                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0"
+                class:zoomed9
+                on:click={toggleZoom9_First}>
                     <p class="w-full h-full p-2 text-center border border-gray-300 resize-none">{ Mixwords[3] }</p>
                 </button>
-                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0">
+                <button class="zoomable w-40 h-20 p-4 border border-gray-300 bg-white m-5 mb-0"
+                class:zoomed10
+                on:click={toggleZoom10_First}>
+
                     <p class="w-full h-full p-2 text-center border border-gray-300 resize-none">{ Mixwords[4] }</p>
                 </button>
             </div>
